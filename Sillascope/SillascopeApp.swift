1095a0c2d8d54b341402218b50c75616372a1cbd
Jeff Blagdon <jeffblagdon@gmail.com>
2024-06-21 09:42:43 -0400

    Initial Commit

---
+++ Sillascope/SillascopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SillascopeApp.swift
+//  Sillascope
+//
+//  Created by Jeffrey Blagdon on 2024-06-21.
+//
+
+import SwiftUI
+
+@main
+struct SillascopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
